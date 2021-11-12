b61d2a0e178f4328a267f7ccc111c11adea6844d
caiocastrofrs <caiocastrofrs@gmail.com>
2021-11-12 19:58:20 -0300

    alterações no banner

--- portfolio/src/components/Banner/index.js
+++ portfolio/src/components/Banner/index.js
@@ -1,21 +1,24 @@
 import '../../sass/layout/banner.scss';
 import sample from './Video/video.mp4';
-import arrowDown from '../img/icons/arrow-down.svg';
+import arrowDown from '../img/icons/arrow-down.svg';    
+
 const Banner = () => {
     return(
         <>
         <section className="banner">
-            {/* <p>Olá, sou o <span className="banner__name">Caio</span> e sou estudante de Desenvolvimento Web</p> */}
-            <span className="banner__text">
+            <p className="banner__introText zoom-in">Olá, sou o <span className="banner__introText__name">Caio</span>. E sou estudante de Desenvolvimento Web</p>
+            <span className="banner__linkText">
                 Mais sobre mim
             </span>
-            <a src="#" className="banner__linkToDown">
-                <arrowDown />
+            
+            <a href="#aboutMe" className="banner__linkToAboutMe">
+               <img src={arrowDown} alt="svg arrow down" className="banner__linkToAboutMe__svg"/>
             </a>
             <video className="banner__video" autoPlay muted loop>
                 <source src={sample} type="video/mp4" />
                 Your browser is not supported!
             </video>
+            
         </section>
         </>
     );
