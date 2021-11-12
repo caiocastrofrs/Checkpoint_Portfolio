f108b63c6db3273bc2470c670ebdf38d953fd8d9
caiocastrofrs <caiocastrofrs@gmail.com>
2021-11-12 19:58:41 -0300

    alterações no header

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -11,8 +11,8 @@ const Header = () => {
             <nav className="header__nav">
               
                 <ul className="header__nav__ul">
-                    <li className="header__nav__ul__item"><a href="#" className="header__nav__ul__item__link">Projetos</a></li>
-                    <li className="header__nav__ul__item"><a href="#" className="header__nav__ul__item__link">Contato</a></li>
+                    <li className="header__nav__ul__item"><a href="#projetos" className="header__nav__ul__item__link">Projetos</a></li>
+                    <li className="header__nav__ul__item"><a href="#contato" className="header__nav__ul__item__link">Contato</a></li>
                 </ul>
             </nav>
         </header>
