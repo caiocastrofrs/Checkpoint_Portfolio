e48f5d0599fd365722b20733d66849109993d688
caiocastrofrs <caiocastrofrs@gmail.com>
2021-11-15 18:23:55 -0300

    testando breakpoints mobile

--- portfolio/src/components/ProjectSection/Project/index.js
+++ portfolio/src/components/ProjectSection/Project/index.js
@@ -13,7 +13,7 @@ const Project = ({title,src, page, repo}) => {
       }, []);
     return(
         <>
-        <div data-aos="fade-up" className="projects__div__project">
+        <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="projects__div__project">
             <img src={src} alt="print of the project"  className="projects__div__project__img"/>
             <div className="projects__div__project__info">
             <h4 className="projects__div__project__info__title">{title}</h4>
